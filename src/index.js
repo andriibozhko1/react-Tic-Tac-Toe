697de62cf2e0b62fe1e0557b0b3aa4858a307721
Andrii <andriibozhko1@gmail.com>
2019-02-20 20:58:09 +0200

    done

--- src/index.js
+++ src/index.js
@@ -21,17 +21,32 @@ class Game extends Component{
 
     this.state = {
       cells: this.fillTable(),
+      crossStatus: [],
+      circleStatus: [],
     }
   }
-  checkWinCombination = (event) => {
+  putValueToArray = (event) => {
     if(event.target.dataset.isEmpty) {
       let id = +event.target.dataset.id;   
-      let newCells = [... this.state.cells];
+      let newCells = [...this.state.cells];
+      let crossStatus = [...this.state.crossStatus ];
+      let circleStatus = [...this.state.circleStatus];
+
       newCells[id].isEmpty = null;
-      this.counter++ % 2 === 0 ? newCells[id].status = 'X' : newCells[id].status = '0';
+      if(this.counter++ % 2 === 0) {
+        newCells[id].status = 'cross'
+        crossStatus.push(id);
+      } else {
+        newCells[id].status = 'circle'
+        circleStatus.push(id);
+      }
 
       this.setState({
         cells: newCells,
+        circleStatus: circleStatus.sort((a,b) => a < b ? -1 : 1),
+        crossStatus: crossStatus.sort((a,b) => a < b ? -1 : 1),
+      },() => {
+        this.checkWinCombination();
       })
     }
   }
@@ -49,24 +64,64 @@ class Game extends Component{
     return cells;
   }
 
+  checkWinCombination = () => {
+    let quantityEmptyCells = 0;
+    this.winCombination.map(combination => {
+      let crossStatus = this.state.crossStatus.filter(item => ~combination.indexOf(item));
+      let circleStatus = this.state.circleStatus.filter(item => ~combination.indexOf(item));
+
+
+      if (JSON.stringify(combination) === JSON.stringify(crossStatus)) {
+        alert("WIN: X");
+        this.restartGame();
+        return;
+      } else if (
+        JSON.stringify(combination) === JSON.stringify(circleStatus)) {
+        alert("WIN: O");
+        this.restartGame();
+        return;
+      } else if(quantityEmptyCells >= this.state.cells.length) {      
+          alert('Draw');
+          this.restartGame();
+          return;
+      }
+    })
+    
+    this.state.cells.map(cell => {
+      if(!cell.isEmpty) {
+        quantityEmptyCells++;
+      }
+    })
+  }
+
+  restartGame = () => {
+    this.counter = 0;
+    this.setState({
+        cells: this.fillTable(),
+        crossStatus: [],
+        circleStatus: [] 
+    })
+  }
+
   render() {
     return(
+    <div>
       <div className="Game">
         {this.state.cells.map((cell, index) => {
           return (
             <div 
             key={index}
-            className="Game__cell"
+            className={`Game__cell ${cell.status ? `Game__cell-${cell.status}` : ''}`}
             data-status={cell.status}
             data-id={cell.id}
             data-is-empty={cell.isEmpty}
-            onClick={this.checkWinCombination}
+            onClick={this.putValueToArray}
             >
-            {cell.status}
             </div>)
-          
         })}
       </div>
+      <div className="Game__restart-btn" onClick={this.restartGame}>RESTART GAME</div>
+    </div>
     )
   }
 }
