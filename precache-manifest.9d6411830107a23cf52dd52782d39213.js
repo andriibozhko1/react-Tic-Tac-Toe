303dfa9fca3a5aea2b6aeacf161f5461846422fb
Andrii <andriibozhko1@gmail.com>
2019-02-21 20:23:10 +0200

    to gh-pages

--- precache-manifest.9d6411830107a23cf52dd52782d39213.js
+++ precache-manifest.9d6411830107a23cf52dd52782d39213.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "e6031a921022611f2a3e",
+    "revision": "a7bd24dd361a51d839d4",
     "url": "/react-Tic-Tac-Toe/static/css/main.0d84fa7c.chunk.css"
   },
   {
-    "revision": "e6031a921022611f2a3e",
-    "url": "/react-Tic-Tac-Toe/static/js/main.e6031a92.chunk.js"
+    "revision": "a7bd24dd361a51d839d4",
+    "url": "/react-Tic-Tac-Toe/static/js/main.a7bd24dd.chunk.js"
   },
   {
     "revision": "845785d3e7e45277f055",
@@ -16,15 +16,15 @@ self.__precacheManifest = [
     "url": "/react-Tic-Tac-Toe/static/js/2.23db0278.chunk.js"
   },
   {
-    "revision": "ff298578df6692210a11924e38c1158f",
-    "url": "/react-Tic-Tac-Toe/static/media/circle.ff298578.svg"
-  },
-  {
     "revision": "bb93e3a2bf87cd2035f1215f4bea8aa3",
     "url": "/react-Tic-Tac-Toe/static/media/cross.bb93e3a2.svg"
   },
   {
-    "revision": "fbe4edbdbf8db761a897698fb45aea98",
+    "revision": "ff298578df6692210a11924e38c1158f",
+    "url": "/react-Tic-Tac-Toe/static/media/circle.ff298578.svg"
+  },
+  {
+    "revision": "2f1d0cab7e9ade61690d48a0ee8dcadf",
     "url": "/react-Tic-Tac-Toe/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-Tic-Tac-Toe/precache-manifest.430a44d39ad11e061527c797c0438c75.js"
+  "/react-Tic-Tac-Toe/precache-manifest.9d6411830107a23cf52dd52782d39213.js"
 );
 
 workbox.clientsClaim();
